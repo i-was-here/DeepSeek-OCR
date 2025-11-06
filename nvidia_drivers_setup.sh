9232155321987eb2ed6e348bcc344eafc1909706
i-was-here <nirmitarora26@gmail.com>
2025-11-06 10:55:20 +0000

    minor update in nvidia driver setup

--- nvidia_drivers_setup.sh
+++ nvidia_drivers_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+sudo apt-get remove --purge 'nvidia-*'
+sudo apt-get autoremove
+sudo apt-get autoclean
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
+
 sudo apt install -y ubuntu-drivers-common
 ubuntu-drivers devices
 sudo apt install -y nvidia-driver-570  # or recommended one
